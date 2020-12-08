1730086dda802521e9690e9cd8873e4c0a5f41cb
stephenpalgeorge <stephenpalgeorge@gmail.com>
2020-12-08 22:55:24 +0000

    update fetch url to include api segment

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,7 @@ import { MemberLookupReturn, LookupError, LookupParams } from '../lib/members';
 export const useLookup = async ({url, searchBy, searchFor, type = 'single'}: LookupParams): Promise<[MemberLookupReturn|null, LookupError|null]> => {
   try {
     const terms: string = searchFor.split(',').join('+');
-    const response = await fetch(`${url}/${searchBy}/${type}/${terms}`);
+    const response = await fetch(`${url}/api/${searchBy}/${type}/${terms}`);
     if (response.status !== 200) {
       const error = await response.json();
       return [null, {...error, status: response.status}];
